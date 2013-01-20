46493411097bee6c3ca9e2687fa26726b9dab371
Chris Wilson <christopher.e.wilson@gmail.com>
2013-01-19 22:13:36 -0500

    removing old files

--- congress/scripts/sample.py
+++ congress/scripts/sample.py
@@ -1,5 +1,6 @@
 import sqlite3, json, os, re
 import nltk
+from collections import defaultdict
 #from nltk import word_tokenize
 
 cwd = os.getcwd()
@@ -14,50 +15,100 @@ def dict_factory(cursor, row):
 conn = sqlite3.connect(cwd + '/congress/data/members.sqlite')
 conn.row_factory = dict_factory
 c = conn.cursor()
-
+    
+c.execute('''
+    CREATE TABLE IF NOT EXISTS jobs
+       ("id" INTEGER PRIMARY KEY AUTOINCREMENT,
+        "pid" VARCHAR(64),
+        "job" VARCHAR(100),
+        "verb" VARCHAR(64),
+        CONSTRAINT "unq" UNIQUE (pid, job, verb))'''
+    )    
+    
 f = open(cwd + "/congress/data/sample.json", 'w')
 
-output = []
+grammar = r"""
+   DATE: {<NNP><CD><,><CD>}
+   DATE: {<CD>}
+   PNP:  {<NNP.*>+}   # chunk determiner/possessive, adjectives and nouns
+   PNP:  {<PNP>+<,|CC|TO|DT|IN>+<PNP>+}
+   PNP:  {<PNP>+<,|CC|TO|DT|IN>+<PNP>+}
+   CNP:  {<JJ.*>?<NN.*>+ }"""
 
-patterns = [
-    ["date", r"((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\w*\.? \d+[ \d]{0,5})"],
-    ["date", r"\b\d{4}\b"],
-]
-
-for sample in c.execute("SELECT * FROM bios ORDER BY Random() LIMIT 5").fetchall():
-    biolines = []
-    for bioline in sample["bio"].replace("\n", " ").split(';'):
-        #get parts of speech for each phrase, and make mutable        
-        pos = map(list, nltk.pos_tag(nltk.word_tokenize(bioline)))
-        print pos
-        sentence = []
-        w = 0
-        while w < len(pos) - 1:
-            #if next word has same grammar type as this word (including plurals)
-            if pos[w][1] == pos[w + 1][1] or pos[w][1] + "S" == pos[w + 1][1] or pos[w][1] == pos[w + 1][1] + "S":
-                pos[w][0] += " " + pos[w + 1][0]
-                pos.pop(w + 1)
-                '''
-            if w < len(pos) - 2 and pos[w + 1][0] == "of" and pos[w][1] == pos[w + 2][1] or pos[w][1] + "S" == pos[w + 2][1] or pos[w][1] == pos[w + 2][1] + "S":
-                pos[w][0] += " " + pos[w + 1][0]
-                pos.pop(w + 1)
-                pos[w][0] += " " + pos[w + 1][0]
-                pos.pop(w + 1)      
-                '''
-            else:
-                w += 1
-        biolines.append(" ".join(['<span class="%s">%s</span> ' % (x[1], x[0]) for x in pos]))
-    o = dict(sample)
-    o["biolines"] = biolines
-    print o["biolines"]
-    output.append(o);
-
-f.write(json.dumps(output, indent=2))
-
-f.close()
-
-
+cp = nltk.RegexpParser(grammar)
     
+def condense(parsed):
+    sentence = []
+    for n in parsed:
+        if isinstance(n, nltk.tree.Tree): 
+            sentence.append((" ".join([x[0] for x in n.leaves()]), n.node))
+        else:
+            sentence.append(n)
+    return sentence
+
+def extract(bio): 
+    insights = []
+    biolines = []
+    for phrase in re.sub("\(.*?\)", "", bio['bio'].encode("ascii", "replace").replace("\n", " ").replace("-", " - ")).split(";"):
+        if len(phrase.strip().split(" ")) == 1:
+            #biolines.append("<span class='CNP'>%s</span>" % phrase.strip())
+            insights.append(("", phrase.strip()))
+            continue
+        parsed = condense(cp.parse(nltk.pos_tag(nltk.word_tokenize(phrase))))
+        verb = ""
+        for n in parsed:
+            if re.match("VB.*?", n[1]):
+                verb = n[0]
+            elif n[1] == "CNP":
+                insights.append((verb, n[0]))
+        
+        #biolines.append(" ".join(["<span class='%s'>%s</span>" % (x[1], x[0]) for x in parsed]))
+    return insights
+
+def sample(M, N):
+    #get five random bios for testing
+    #f = open(cwd + "/congress/data/sample.json", 'w')
+    output = []
+    #for sample in c.execute("SELECT * FROM bios ORDER BY Random() LIMIT 5").fetchall():
+    for sample in c.execute("SELECT pid, bio FROM bios LIMIT %d OFFSET %d" % (M, N)).fetchall():
+        print sample['pid']
+        biolines = []
+        insights = []
+        for phrase in re.sub("\(.*?\)", "", sample['bio'].encode("ascii", "replace").replace("\n", " ").replace("-", " - ")).split(";"):
+            if len(phrase.strip().split(" ")) == 1:
+                biolines.append("<span class='CNP'>%s</span>" % phrase.strip())
+                insights.append(("", phrase.strip()))
+                continue
+            parsed = condense(cp.parse(nltk.pos_tag(nltk.word_tokenize(phrase))))
+            verb = ""
+            for n in parsed:
+                if re.match("VB.*?", n[1]):
+                    verb = n[0]
+                elif n[1] == "CNP":
+                    insights.append((verb, n[0]))
+            
+            biolines.append(" ".join(["<span class='%s'>%s</span>" % (x[1], x[0]) for x in parsed]))
+            for insight in insights:
+                c.execute('''INSERT OR IGNORE INTO jobs (pid, job, verb) VALUES (?,?,?)''', (sample['pid'], insight[0], insight[1]))
+
+        '''
+        o = dict(sample)
+        o["biolines"] = biolines
+        o["insights"] = insights
+        #print o["biolines"]
+        output.append(o);
+        '''
+    conn.commit()
+    #f.write(json.dumps(output, indent=2))    
+    #f.close()
+
+co = 8000;
+while co < 13000:
+    sample(10, co + 10)
+    print co
+    co += 10
+    
+   
     
 #conn.commit()
 conn.close()
--- congress/scripts/test.py
+++
@@ -1,4 +0,0 @@
-import nltk
-text = nltk.word_tokenize("And now for something completely different")
-
-print nltk.pos_tag(text)
